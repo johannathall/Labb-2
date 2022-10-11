b23c48e8d73dc6b151cbc36d5d083c5b95dcbc11
johannathall <32735219+johannathall@users.noreply.github.com>
2022-10-11 16:09:03 +0200

    add app service

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,44 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.25.0"
     }
   }
 }
 
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+provider "azurerm" {
+  features {}
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
+locals {
+  resource_group_name   = "rg-${var.environment}-${var.location}-${var.name}"
+  app_service_plan_name = "asp-${var.environment}-${var.location}-${var.name}"
+  app_service_name      = "wa-${var.environment}-${var.location}-${var.name}"
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = local.resource_group_name
+  location = var.location_long
+}
+
+resource "azurerm_service_plan" "this" {
+  name                = local.app_service_plan_name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "this" {
+  name                = local.app_service_name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  service_plan_id     = azurerm_service_plan.this.id
+
+  site_config {
+    application_stack {
+      node_version = "16-lts"
+    }
   }
 }
-
